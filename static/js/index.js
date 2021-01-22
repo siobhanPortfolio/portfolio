08e98f87ee7abe6497f2d9caa495b1e2739dcadd
Ciaran Whyte <ciaran.whyte@fireeye.com>
2021-01-22 15:44:36 +0000

    scroll and snap

--- static/js/index.js
+++ static/js/index.js
@@ -1,30 +1,18 @@
-const sections = [...document.querySelectorAll("section")];
-
-let options = {
-  rootMargin: "0px",
-  threshold: 0.75
+TweenLite.set('body', { perspective: 700 });
+var slides = document.querySelectorAll('.slide'), tl = new TimelineLite({ paused: true });
+for (var i = 0; i < slides.length; i++) {
+    //if(i!=0){tl.addPause('pause'+i)};
+    if (i != slides.length - 1) {
+        tl.to(slides[i], 0.7, { yPercent: -100 })
+            .from(slides[i + 1], 0.7, { yPercent: 100 }, '-=0.7')
+            .from(slides[i + 1].getElementsByTagName('span'), 1, { y: '100vh', ease: Back.easeInOut.config(2) }, '-=0.3')
+            .addPause('pause' + i)
+    };
 };
 
-const callback = (entries, observer) => {
-  entries.forEach(entry => {
-		const { target } = entry;
-		
-		if (entry.intersectionRatio >= 0.75) {
-			target.classList.add("is-visible");
-		} else {
-			target.classList.remove("is-visible");
-		}
-  });
+function GO(e) {
+    var SD = isNaN(e) ? e.wheelDelta || -e.detail : e;
+    if (SD < 0) { tl.play() } else { tl.reverse() };
 };
-
-const observer = new IntersectionObserver(callback, options);
-
-sections.forEach((section, index) => {
-  const sectionChildren = [...section.querySelector("[data-content]").children];
-
-  sectionChildren.forEach((el, index) => {
-    el.style.setProperty("--delay", `${index * 250}ms`);
-  });
-
-  observer.observe(section);
-});
+document.addEventListener("mousewheel", GO);
+document.addEventListener("DOMMouseScroll", GO);
