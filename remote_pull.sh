340835912edbe4700c5ec84eaf6931ccc52bda35
Ciaran Whyte <ciaran.whyte@fireeye.com>
2021-01-22 14:50:48 +0000

    adding remote deploy script

--- remote_pull.sh
+++ remote_pull.sh
@@ -1,6 +1,5 @@
 ssh -i  ~/.ssh/siobhan_homepage.pem ec2-user@52.87.154.239 << EOF
-  ls some_folder; 
-  ./someaction.sh 'some params'
-  pwd
-  ./some_other_action 'other params'
+  cd /home/ec2-user/portfolio
+  git pull origin main
+  echo "Exiting with $?"
 EOF
