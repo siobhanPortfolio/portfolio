1f71957be792bacb9a836de62abf2e66adc59830
Ciaran Whyte <ciaran.whyte@fireeye.com>
2021-01-22 15:38:51 +0000

    new site

--- static/js/index.js
+++ static/js/index.js
@@ -1,26 +1,30 @@
 const sections = [...document.querySelectorAll("section")];
 
 let options = {
-    rootMargin: "0px",
-    threshold: 0.25
+  rootMargin: "0px",
+  threshold: 0.75
 };
 
 const callback = (entries, observer) => {
-    entries.forEach(entry => {
-        const { target } = entry;
-        console.log(entry, target)
-
-        if (entry.intersectionRatio >= 0.25) {
-            target.classList.add("is-visible");
-        } else {
-            target.classList.remove("is-visible");
-        }
-    });
+  entries.forEach(entry => {
+		const { target } = entry;
+		
+		if (entry.intersectionRatio >= 0.75) {
+			target.classList.add("is-visible");
+		} else {
+			target.classList.remove("is-visible");
+		}
+  });
 };
 
 const observer = new IntersectionObserver(callback, options);
 
 sections.forEach((section, index) => {
-    observer.observe(section);
-});
+  const sectionChildren = [...section.querySelector("[data-content]").children];
 
+  sectionChildren.forEach((el, index) => {
+    el.style.setProperty("--delay", `${index * 250}ms`);
+  });
+
+  observer.observe(section);
+});
