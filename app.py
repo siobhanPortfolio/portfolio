096192d87b7410bc757369641f1f4e979b7da5fd
Ciaran Whyte <ciaran.whyte@fireeye.com>
2021-01-22 15:18:46 +0000

    adding something to look at

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! edit'
+    render_template("index.html")
 
 app.run(host='0.0.0.0', port='81', debug=True)
