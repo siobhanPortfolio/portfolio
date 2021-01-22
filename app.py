e7f1aefcf4d935c9993051919e937b8a8285ba65
Ciaran Whyte <ciaran.whyte@fireeye.com>
2021-01-22 15:21:31 +0000

    bug fix

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    render_template("index.html")
+    return render_template("index.html")
 
 app.run(host='0.0.0.0', port='81', debug=True)
