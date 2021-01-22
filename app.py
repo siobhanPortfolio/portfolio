3293184c57b2571c01e4eaf1dfa2c24341f9116b
Ciaran Whyte <ciaran.whyte@fireeye.com>
2021-01-22 15:25:42 +0000

    adding some javascript

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template("index.html")
 
 app.run(host='0.0.0.0', port='81', debug=True)
